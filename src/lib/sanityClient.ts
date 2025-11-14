ad737ce95e5de98e469b5705a938fa1e158fee9f
franvinkus <vincentkurnia29@gmail.com>
2025-11-14 11:40:14 +0700

    changed projectId and dataset to matched with vercel configuration

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,12 +1,12 @@
 import { createClient } from "next-sanity";
 
-export const projectId = 'tjpldp3n'
-export const dataset = 'production'
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
 const apiVersion = '2025-11-10'
 
 export const sanityClient = createClient({
     projectId,
     dataset,
     apiVersion,
-    useCdn: false
+    useCdn: true
 })
