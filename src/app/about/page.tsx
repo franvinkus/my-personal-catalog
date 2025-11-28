878c68cb813d176e5cb9d3fe0458b648c99faae7
franvinkus <vincentkurnia29@gmail.com>
2025-11-28 10:06:11 +0700

    Changed theme from memes to jewelery, Added Fonts, Deleted unused pictures

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,23 +3,18 @@ import Navbar from "@/component/navbar";
 
 export default function About(){
     return(
-        <div className="h-full">
+        <div className="min-h-screen flex flex-col">
             <Navbar/>
-            <div className="mt-[120px] mb-5">
+            <div className="mt-[200px] mb-5 grow">
                 <div className="flex justify-center text-4xl">
-                   About
+                   About Us
                 </div>
-                <div>
-                    <label className="flex justify-center text-xl">
-                        Guk. Guk guk. (Terjemahan: Elegan bertemu pesona harian.) <br/>
+                <div className="flex font-sans w-full justify-center align-middle">
+                    <label className="flex w-250 text-xl ">
+                        At Elewery, we believe true elegance doesn't need to be complicated. Our philosophy is simple: to create jewelry that enhances your style, rather than overshadowing it. <br/><br/><br/>
+                        Every piece in our collection is thoughtfully designed for the modern woman who values detail. From morning meetings to evening dates, Elewery is here to add just the perfect touch of sparkle—anytime, anywhere. <br/><br/><br/>
+                        Elewery. Elegant in every way.
 
-                        Woof woof bark, awoo! (Terjemahan: Kami percaya aksesori adalah ekspresi diri.)<br/>
-
-                        Sniff sniff... grrr... tail wag. (Terjemahan: Desain simpel, nyaman, dan hangat.)<br/>
-
-                        Bark bark! Zoomies! (Terjemahan: Cocok buat acara formal maupun santai.)<br/>
-
-                        Awoo. (Terjemahan: Elegan dan apa adanya.)<br/>
                     </label>
 
                 </div>
--- src/component/productCard.tsx
+++ src/component/productCard.tsx
@@ -12,13 +12,13 @@ export default function ProductCard({name, details, price, imageUrl} : product){
 
     return(
         <div className="flex p-5 border-2 rounded-2xl flex-col hover:shadow-2xl hover:scale-110">
-            <div className="flex justify-center">
-                <img src={imageUrl} className="h-[170px] w-[150px] rounded-2xl" />
+            <div className="flex justify-center h-72 w-full">
+                <img src={imageUrl} className="h-full w-full rounded-2xl" />
             </div>
 
-            <div className="flex flex-col items-center">
-                <label>{name}</label>
-                <label>{details}</label>
+            <div className="flex flex-col grow items-center space-y-3">
+                <label className="line-clamp-1">{name}</label>
+                <label className="line-clamp-2">{details}</label>
                 <label>{price.toLocaleString('id-ID')}</label>
             </div>
 
